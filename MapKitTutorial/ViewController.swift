fb6626631b1ce5bc9dcb2fc75dd4c2e73aca5372
Mariem Ayadi <mayadi@smith.edu>
2014-11-08 17:01:07 -0500

    Add lat/long coordinates and define location specifications

--- MapKitTutorial/ViewController.swift
+++ MapKitTutorial/ViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // A Map kit needs to have certain values
+        
+        //input known location
+        var latitude:CLLocationDegrees = 48.399193
+        var longitude:CLLocationDegrees = 9.993341
+        
+        var latitudeDelta:CLLocationDegrees = 0.01
+        var longitudeDelta:CLLocationDegrees = 0.01
+        
+        //A structure that defines the area spanned by a map region.
+        var theSpan:MKCoordinateSpan = MKCoordinateSpanMake(latitudeDelta, longitudeDelta)
+    
+        //Structure containing a geographical coordinate
+        var buildingLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        //A structure that defines which portion of the map to display. (center coordinate + span)
+        var regionOfInterest:MKCoordinateRegion = MKCoordinateRegionMake(buildingLocation, theSpan)
+        
     }
 
     override func didReceiveMemoryWarning() {

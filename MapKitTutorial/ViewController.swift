818adef4ea76e0f8fff2c91c466a5cfd6f37101a
Mariem Ayadi <mayadi@smith.edu>
2014-11-08 15:49:20 -0500

    Set base code for Map Kit tutorial

---
+++ MapKitTutorial/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MapKitTutorial
+//
+//  Created by Mariem Ayadi on 11/8/14.
+//  Copyright (c) 2014 Mariem Ayadi. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

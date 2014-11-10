4c6da1f7ddc6d7238b1eb24cc45cbd1e10b0b929
Mariem Ayadi <mayadi@smith.edu>
2014-11-10 16:00:02 -0500

    Show default. Call back working.

--- MapKitTutorial/ViewController.swift
+++ MapKitTutorial/ViewController.swift
@@ -10,29 +10,60 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate{
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     //NOTE:
     //The CLLocationManagerDelegate protocol defines the methods used to receive location and heading updates from a CLLocationManager object.
     
     let locationManager = CLLocationManager()
+    
+    //Have Lat and Long be global.
+    //Default is Ford Hall
+    //var lat:CLLocationDegrees = 42.315904
+    //var long:CLLocationDegrees = -72.637915
+    var lat:CLLocationDegrees = 19
+    var long:CLLocationDegrees = 99.13333
+    
     //NOTE:
     //The CLLocationManager class is the central point for configuring the delivery of location- and heading-related events to your app. You use an instance of this class to establish the parameters that determine when location and heading events should be delivered and to start and stop the actual delivery of those events. You can also use a location manager object to retrieve the most recent location and heading data.
 
-    @IBOutlet weak var mapView: MKMapView! 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        println("Entered the function")
+        
+        //NOTE:
+        //The CLGeocoder class provides services for converting between a coordinate (specified as a latitude and longitude) and the user-friendly representation of that coordinate.
+        long = manager.location.coordinate.longitude
+        lat = manager.location.coordinate.latitude
+        
+        println("Assignment happended")
+        println(long)
+        println(lat)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // A Map kit needs to have certain values
         
+        println("inside viewDidLoad")
+        println(long)
+        println(lat)
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        //new for iOS8. We may add a description as to why we would like to add a user's description
+        self.locationManager.startUpdatingLocation()
         
         //input known location
-        //var latitude:CLLocationDegrees = 48.399193
-        //var longitude:CLLocationDegrees = 9.993341
-        
-        //Ford Hall
-        var latitude:CLLocationDegrees = 42.315904
-        var longitude:CLLocationDegrees = -72.637915
+        var latitude:CLLocationDegrees = lat
+        var longitude:CLLocationDegrees = long
+
         
         var latitudeDelta:CLLocationDegrees = 0.01
         var longitudeDelta:CLLocationDegrees = 0.01
